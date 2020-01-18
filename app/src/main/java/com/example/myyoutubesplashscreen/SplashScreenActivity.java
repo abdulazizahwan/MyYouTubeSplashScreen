7d00e5927b4896edce1538b3078e72ab8d5bd799
abdulazizahwan <abdulazizahwan97@gmail.com>
2020-01-18 16:10:45 +0700

    first commit

---
+++ app/src/main/java/com/example/myyoutubesplashscreen/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.myyoutubesplashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //remove title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        },2000L); //2seconds
+    }
+}
